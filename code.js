f1b7513142e8c51c01289b87bb0f2eda5fd9aae3
Core <carljeffersonlay@gmail.com>
2023-07-20 19:19:54 +0800

    Fill up playRound()

--- code.js
+++ code.js
@@ -3,23 +3,35 @@
 function getComputerChoice(){
     const choose = Math.floor(Math.random()*3);
     // 2 = scissors, 1 = paper, 0 = rock
-    const choice = (choose === 2) ? 'scissors' : (choose ? 'paper' : 'rock');
+    const choice = (choose === 2) ? 'Scissors' : (choose ? 'Paper' : 'Rock');
     return choice;
 }
 
-console.log(getComputerChoice());
-// // Return string on who wins and what choices were made
-// function playRound(playerSelection, computerSelection) {
+// Return string on who wins and what choices were made
+function playRound(playerSelection, computerSelection) {
+    const result = '';
+    let winner = '';
 
-// }
+    if (playerSelection === computerSelection) result = 'Draw! Both picked ${playerSelection}';
+    else if (playerSelection === 'Rock') winner = (computerSelection === 'Paper') ? 'Computer' : 'Player';
+    else if (playerSelection === 'Paper') winner = (computerSelection === 'Scissors') ? 'Computer' : 'Player';
+    else if (playerSelection === 'Scissors') winner = (computerSelection === 'Rock') ? 'Computer' : 'Player';
 
-// let playerSelection = ''; // Get user input
-// let computerSelection = getComputerChoice();
-// console.log(playRound(playerSelection, computerSelection));
+    if (winner === 'Player') {
+        result = '${winner} wins! ${computerSelection} wins against ${playerSelection}'
+    }
+    result = (winner === 'Player') ? '${winner} wins! ${playerSelection} wins against ${computerSelection}' : '${winner} wins! ${computerSelection} wins against ${playerSelection}';
+    
+    return [result, winner]; 
+}   
 
-// // 5 rounds, that keeps track of scores. Shows winner at the end
-// function game() {
-//     for (let i = 0; i < 5; i++) {
-//         // Code
-//     }
-// }
+let playerSelection = ''; // Get user input
+let computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+// 5 rounds, that keeps track of scores. Shows winner at the end
+function game() {
+    for (let i = 0; i < 5; i++) {
+        // Code
+    }
+}
