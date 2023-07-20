d49e49a0c5656045813832ae630d2bbaf71c1502
Core <carljeffersonlay@gmail.com>
2023-07-20 18:52:14 +0800

    Add basic structure to algo

---
+++ code.js
@@ -0,0 +1,21 @@
+
+// Randomnly return 'rock', 'paper', or 'scissors'
+function getComputerChoice(){
+
+}
+
+// Return string on who wins and what choices were made
+function playRound(playerSelection, computerSelection) {
+
+}
+
+let playerSelection = ''; // Get user input
+let computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+// 5 rounds, that keeps track of scores. Shows winner at the end
+function game() {
+    for (let i = 0; i < 5; i++) {
+        // Code
+    }
+}
