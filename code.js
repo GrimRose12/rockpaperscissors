25dec2b2aa0bb38b29151bd9ee88b6fd8fc1d118
Core <carljeffersonlay@gmail.com>
2023-07-20 18:59:41 +0800

    Fill up getComputerChoice()

--- code.js
+++ code.js
@@ -1,21 +1,25 @@
 
 // Randomnly return 'rock', 'paper', or 'scissors'
 function getComputerChoice(){
-
+    const choose = Math.floor(Math.random()*3);
+    // 2 = scissors, 1 = paper, 0 = rock
+    const choice = (choose === 2) ? 'scissors' : (choose ? 'paper' : 'rock');
+    return choice;
 }
 
-// Return string on who wins and what choices were made
-function playRound(playerSelection, computerSelection) {
+console.log(getComputerChoice());
+// // Return string on who wins and what choices were made
+// function playRound(playerSelection, computerSelection) {
 
-}
+// }
 
-let playerSelection = ''; // Get user input
-let computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+// let playerSelection = ''; // Get user input
+// let computerSelection = getComputerChoice();
+// console.log(playRound(playerSelection, computerSelection));
 
-// 5 rounds, that keeps track of scores. Shows winner at the end
-function game() {
-    for (let i = 0; i < 5; i++) {
-        // Code
-    }
-}
+// // 5 rounds, that keeps track of scores. Shows winner at the end
+// function game() {
+//     for (let i = 0; i < 5; i++) {
+//         // Code
+//     }
+// }
